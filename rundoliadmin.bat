2d6481e47b7895bbf6e1739581de1f6088477ebb
Alex <alrope1@bemen3.cat>
2017-05-05 14:15:12 +0200

    dolibarr

---
+++ rundoliadmin.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Launch Dolibarr Admin center
+REM ----------------------------
+
+REM Go to admin center page
+start "DoliWamp admin" "C:\Program Files (x86)/Mozilla Firefox/firefox.exe" http://localhost:80/
